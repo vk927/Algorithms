6fd7e1f7dc9132cfba82cd90e5048c8bdd4935a3
venkata_kalluri <vk927@nyu.edu>
2017-01-27 12:36:45 -0500

    commitin ...

---
+++ src/LinkedList/demo.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+/**
+ * Created by venkata_kalluri on 1/27/2017.
+ */
+public class demo {
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
+}
